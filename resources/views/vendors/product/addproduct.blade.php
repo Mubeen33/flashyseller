5b14e0198ff0d368cbc519cb426394eb0ee7d7d1
Mubeen <mubeen33@gmail.com>
2020-08-27 14:47:09 +0500

    Mubeen

--- resources/views/vendors/product/addproduct.blade.php
+++ resources/views/vendors/product/addproduct.blade.php
@@ -421,6 +421,78 @@
 @section('script')
     <script src = "https://code.jquery.com/jquery-1.10.2.js"></script>
     <script src = "https://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
-    <script type="text/javascript" src="{{ asset('js/index.js') }}"></script>
+    {{-- <script type="text/javascript" src="{{ asset('js/index.js') }}"></script> --}}
+    <script type="text/javascript">
+$(document).ready(function(){
+    $("[product='img']").click(function(){
+        var n = $(this).attr('n');
+        var src = $(this).attr('src');
+        $("#pimg"+n).click();
+        $("#pimg"+n).change(function(event) {    
+          var formData = new FormData();
+          var file = document.getElementById("pimg"+n).files[0];
+          formData.append("Filedata", file);
+          var t = file.type.split('/').pop().toLowerCase();
+          const fsize = file.size;
+          var tmppath = URL.createObjectURL(event.target.files[0]);
+            //$("img").fadeIn("fast").attr('src',tmppath);
+          $("#img"+n).attr('src',src.replace(src,tmppath)); 
+        });
+    });
+});
+$(document).ready(function(){
+    $("#ui-id-1").addClass("list-group");
+              $(function() {
+        var projects = [
+           {
+              value: "Phone Cs in phone Cases",
+              desc: "<p class='p-graph'> Home & living ▸ Home Decor ▸ wall decor </p>",
+           },
+           {
+              value: "Iphone Mobile",
+              desc: "<p class='p-graph'> Electornics ▸ Mobile phone ▸ Iphone</p>",
+           },
+           {
+              value: "wall decorators",
+              desc: "<p class='p-graph'> Home & living ▸ Home Decor ▸ wall decor </p>",
+           },
+           {
+              value: "1 flat",
+              desc: "<p class='p-graph'>▸ Property ▸ Home ▸ flat </p>",
+           },
+           {
+              value: "Phone Cs in phone Cases",
+              desc: "<p class='p-graph'> Home & living ▸ Home Decor ▸ wall decor </p>",
+           },
+           {
+              value: "Phone Cs in phone Cases",
+              desc: "<p class='p-graph'> Home & living ▸ Home Decor ▸ wall decor </p>",
+           },
+        ];
+        $( "#project" ).autocomplete({
+           minLength: 0,
+           source: projects,
+           focus: function( event, ui ) {
+              $( "#project" ).val( ui.item.label );
+                 return false;
+           },
+           select: function( event, ui ) {
+              $( "#project" ).val( ui.item.label );
+              $( "#project-id" ).val( ui.item.value );
+              $( "#project-description" ).html( ui.item.desc );
+              return false;
+           }
+        })
+    
+        .data( "ui-autocomplete" )._renderItem = function( ul, item ) {
+           return $( "<li class='list-group-item auto-list list-group-item-action'>" )
+           .append( "<a style='text-decoration:none;'>" + item.label + "<br>" + item.desc + "</a>" )
+           .appendTo( ul );
+        };
+        $("#ui-id-1").addClass("list-group");
+        (".ui-helper-hidden-accessible").remove();
+     });
+});
+    </script>
 @endsection
     
