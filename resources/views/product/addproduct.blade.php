a89951a86551c2d9f34b06cf87606a561aa93f7e
Mubeen <mubeen33@gmail.com>
2020-09-11 16:55:57 +0500

    Mubeen

--- resources/views/product/addproduct.blade.php
+++ resources/views/product/addproduct.blade.php
@@ -52,7 +52,7 @@
 	color      : #373738 !important;
   }
   .dropzone .dz-message{
-	font-size  : 1rem !important;
+	font-size  : 0.85rem !important;
 	color      : #373738 !important;
 	position: absolute;
 	z-index: 9;
