61cf09ea8ecbcc60b8aecfe3629f9f2805ce5be5
nafeeur10 <nafeeur.kuet.cse6@gmail.com>
2020-09-12 16:40:53 +0600

    Nafeeur

--- public/app-assets/js/scripts/extensions/variants.js
+++ public/app-assets/js/scripts/extensions/variants.js
@@ -3,37 +3,36 @@ const { has } = require("lodash");
 function getVariantOption(variation_id) {
 
     let li_counter = 0;
+    let li_counter3 = 0;
+
     var secondVariations;
 
 	if (variation_id !== "") {
+
+        variantcounter = variantcounter + 1;
+
 		$.ajax({
 			url:"/vendor/ajax-get-variant-options/fetch?variation_id="+variation_id,
 			method:'GET',
 			cache:false,
-			success:function(response){
+			success:function(response) {
 
-                variantcounter++;
+                //variantcounter++;
 
-                //console.log(response);
                 secondVariations = response.second_variations;
                 let variationName = response.variationName;
                 let variationList = response.variationList;
-                //console.log("List: ", variationList);
 
                 let variationListTargettedValue = variationList.filter(obj => {
                     return obj.variation_name === variationName
                 });
 
-               // console.log(variationListTargettedValue);
-
                 var html = "<div class='my-2 row w-100' id='variantid" + variation_id + "'>";
                 html += "<div class='col-md-4'><b class='mr-2'>" + variationName;
-                html+= "</b><button class='btn btn-default btn-remove' >Delete</button>";
-                html += "<br><div><div class='form-check'><input type='checkbox' class='form-check-input d-block' style='width: auto;'/><label class='form-check-label'>Image</label></div>";
-                html += "<div class='form-check'><input type='checkbox' class='form-check-input d-block' style='width: auto;'/><label class='form-check-label'>SKU</label></div>";
-                html += "</div>";
-                html += "</div>";
-                html += "<div class='col-md-4'>";
+                html+= "</b>";
+                html+="<button class='btn btn-default text-decoration-underline btn-edit" + variation_id + "' style='display: none;'>Edit unit</button>"
+                html+="<button class='btn btn-default btn-remove text-decoration-underline'>Delete</button>";
+
                 if(variationListTargettedValue[0].is_select === 1) {
                 html += "<select class='form-control second-variant-selectbox' id='variant-select2" + variation_id + "'>";
                 html += "<option value='-1'>Select An Option</option>";
@@ -50,21 +49,25 @@ function getVariantOption(variation_id) {
                     html += "<div><ul class='variant2List sortable' id='sortable" + variation_id + "'></ul></div>";
                 }
 
-                html += "</div><div class='col-md-4'>";
+                html += "<br><div><div class='form-check'><input type='checkbox' class='form-check-input d-block' style='width: auto;'/><label class='form-check-label'>Image</label></div>";
+                html += "<div class='form-check'><input type='checkbox' class='form-check-input d-block' style='width: auto;'/><label class='form-check-label'>SKU</label></div>";
+                html += "</div>";
+                html += "</div>";
+                html += "<div class='col-md-4'>";
                 html += "<div><ul class='variant2List sortable' id='sortable3" + variation_id + "'></ul></div>";
+                html += "</div><div class='col-md-4'>";
                 html += "</div></div>";
-                II = II + 1;
 
-                console.log("In Adding: ", variantcounter);
+        
                 if(variantcounter>=2) {
                     $("#addVariantButton").hide();
                     $("#render__variations__data").hide();
                     variantcounter = 2;
                 }
 
+                //$(".btn-edit").hide();
 
                 $("#loadSecondVariationOptionsData").append(html);
-                
 			},
 		});
 	
@@ -82,12 +85,11 @@ function getVariantOption(variation_id) {
         $("#"+button_id).remove();  
 
         variantcounter--;
-        //console.log("After Delete Variant: ", variantcounter);
 
         $("#addVariantButton").show();
         $("#render__variations__data").show();
 
-        variantcounter++;
+        //variantcounter++;
      }); 
      
      
@@ -109,6 +111,12 @@ function getVariantOption(variation_id) {
     });
 
 
+    $(document).on('click', '.btn-edit' + variation_id, function(e){
+        e.preventDefault();
+        $(this).hide();
+        $("#variant-select2"+variation_id).show();
+    });
+
     // Remove Sub Variant From the List 
 
     $(document).on('click', '.remove-variant2', function(e){
@@ -125,6 +133,8 @@ function getVariantOption(variation_id) {
 
         let value_of_select = this.value;
         var hasThirdVariation = [];
+        $("#variant-select2"+variation_id).hide();
+        $(".btn-edit" + variation_id).css('display', 'inline-block');
 
         $.ajax({
             url:"/vendor/ajax-get-variant-options-options/fetch?variation_id="+value_of_select,
@@ -133,23 +143,38 @@ function getVariantOption(variation_id) {
 			success:function(response){
                 hasThirdVariation = response.variation3;
                 
-                if(hasThirdVariation.length > 0) {
+                    if(hasThirdVariation.length > 0) 
+                    {
+                        let htmlvariant3 = '';
+                        htmlvariant3 += "<select class='form-control' id='variant3FinalSelect" + value_of_select + "'>";
+                        
 
-                    let htmlvariant3 = '';
-                    htmlvariant3 += "<select class='form-control' id='variant3FinalSelect" + value_of_select + "'>";
-                    
-
-                    htmlvariant3 += "<option value='-1'>Select An Option</option>";
-                    hasThirdVariation.forEach(element => {
+                        htmlvariant3 += "<option value='-1'>Select An Option</option>";
+                        hasThirdVariation.forEach(element => {
                         htmlvariant3 += "<option value="; 
                         htmlvariant3 +=  element.id + ">" + element.option_name });
                         htmlvariant3 += "</option>";
                         htmlvariant3+="</select>";
                         htmlvariant3 += "<ul class='sortable' id='sortableFinalUL" + value_of_select + "'>"
-                        $("#sortable3"+variation_id).append(htmlvariant3);
+                        $("#sortable3"+variation_id).html(htmlvariant3);
                    }
                    else {
-                        $("#sortable3"+variation_id).append('');
+
+                        let htmlVariant3Input = '';
+                        htmlVariant3Input += "<div class='d-flex'><input type='text' class='form-control placeholder-class variation3value" + variation_id + value_of_select +"' />";
+                        htmlVariant3Input += "<button class='btn btn-info variation-add-button border-rad-0 varation3add" + variation_id + value_of_select + "'>Add</button></div>";
+                        htmlVariant3Input += "<div><ul class='variant3List sortable' id='sortable4" + variation_id + "'></ul></div>";
+                    
+                        $("#sortable3"+variation_id).html(htmlVariant3Input);
+                        let placeholderText;
+
+                        for(let T = 0; T<secondVariations.length; T++) {
+                            if(secondVariations[T].id == value_of_select) {
+                                placeholderText = secondVariations[T].option_name;
+                            }
+                        }
+
+                        $(".variation3value" + variation_id + value_of_select).attr("placeholder", placeholderText);
                     }
                 }
                 
@@ -193,31 +218,47 @@ function getVariantOption(variation_id) {
 
         });
         
-        //var option_value_name;
 
-        // for(var m = 0; m < secondVariations.length; m++) {
-        //     let ourSelect2Obj = secondVariations[m];
-        //     if(ourSelect2Obj.id == value_of_select) {
-        //         option_value_name = ourSelect2Obj.option_name;
-        //     }
-        // }
+        $(document).on('click', '.varation3add' + variation_id + value_of_select, function(e){
+            e.preventDefault();
 
-        // if(value_of_select>0) {
-        //     li_counter++; 
-        //     let htmlVariant = '';
+            let valv3 = $(".variation3value" + variation_id + value_of_select).val();
+            li_counter3++;
+
+            let placeholderText;
+
+            for(let T = 0; T<secondVariations.length; T++) {
+                if(secondVariations[T].id == value_of_select) {
+                    placeholderText = secondVariations[T].option_name;
+                    break;
+                }
+            }
             
-        //     htmlVariant += "<li class='my-1' id='variantLi" + variation_id + li_counter + "'>";
-        //     htmlVariant += "<div class='d-flex'><span class='px-1 py-05 border w-100'>" +  option_value_name + "</span>";
-        //     htmlVariant += "<button class='btn btn-sm border-rad-0 border remove-variant2'>X</button></div></li>";
+            let htmlVariant = '';
             
-        //     $("#sortable-select" + variation_id).append(htmlVariant);
-        // }
+            htmlVariant += "<li class='my-1' id='variant3Li" + variation_id + value_of_select + li_counter3 + "'>";
+            htmlVariant += "<div class='d-flex'><span class='px-1 py-05 border w-100'>" +  valv3 + " "+ placeholderText + "</span>";
+            htmlVariant += "<button class='btn btn-sm border-rad-0 border remove-variant4'>X</button></div></li>";
+            
+            $("#sortable4" + variation_id).append(htmlVariant);
+            $(".variation3value" + variation_id + value_of_select).val('');
+        });
+
+
+        $(document).on('click', '.remove-variant4', function(e){
+            e.preventDefault();
+            let deleteLiItem = $(this).parent().parent().attr('id');
+            $("#" + deleteLiItem).remove();
+
+        });
     });
     
     $( "#sortable" + variation_id ).sortable();
     $( "#sortable"  + variation_id ).disableSelection();
     $( "#sortable-select"  + variation_id ).sortable();
     $( "#sortable-select"  + variation_id ).disableSelection();
+    $( "#sortable4"  + variation_id ).sortable();
+    $( "#sortable4"  + variation_id ).disableSelection();
 }
 
 
