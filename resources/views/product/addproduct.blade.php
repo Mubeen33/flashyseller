dda255177470a214e09ef0abb8f973315f5c843e
Mubeen <mubeen33@gmail.com>
2020-09-12 15:15:37 +0500

    mubbenn

--- resources/views/product/addproduct.blade.php
+++ resources/views/product/addproduct.blade.php
@@ -413,7 +413,7 @@
 								  	</button>
                             </div>
                             <div class="col-lg-2">
-                                {{-- <button type="submit" class="btn btn-warning">Submit</button> --}}
+                                <button type="submit" class="btn btn-warning">Submit</button>
                             </div>
           					</div>
                             
