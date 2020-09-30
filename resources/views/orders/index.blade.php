678d2a4b5eda8aea8aa71147115f58a8879a7ccb
RI Rijan <38884716+DeveloperRijan@users.noreply.github.com>
2020-09-30 16:40:24 +0600

    developer rijan

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -53,12 +53,14 @@
                                                     <th>Customer</th>
                                                     <th>Product</th>
                                                     <th>Category</th>
-                                                    <th>MK Price</th>
-                                                    <th>Selling Price</th>
+                                                    <th title="Market Price">MK Price</th>
+                                                    <th title="Selling Price">Price</th>
                                                     <th>Image</th>
-                                                    <th>Order QTY</th>
-                                                    <th class="sortAble" sorting-column='created_at' sorting-order='DESC'><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-up" fill="currentColor" xmlns="http://www.w3.org/2000/svg"> <path fill-rule="evenodd" d="M8 3.5a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5z"/> <path fill-rule="evenodd" d="M7.646 2.646a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8 3.707 5.354 6.354a.5.5 0 1 1-.708-.708l3-3z"/> </svg>
-                                                        Order Date
+                                                    <th title="Order Quantity">QTY</th>
+                                                    <th title="Grand Total">Total</th>
+                                                    <th title="Payment Option/Method">Payment</th>
+                                                    <th title="Order Date" class="sortAble" sorting-column='created_at' sorting-order='DESC'><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-up" fill="currentColor" xmlns="http://www.w3.org/2000/svg"> <path fill-rule="evenodd" d="M8 3.5a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5z"/> <path fill-rule="evenodd" d="M7.646 2.646a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8 3.707 5.354 6.354a.5.5 0 1 1-.708-.708l3-3z"/> </svg>
+                                                        Date
                                                     </th>
                                                     <th>Status</th>
                                                     <th>Actions</th>
--- resources/views/orders/partials/orders-list.blade.php
+++ resources/views/orders/partials/orders-list.blade.php
@@ -21,12 +21,12 @@
     </td>
     <td>
         @if($vendor_product)
-            {{$vendor_product->mk_price}}
+            {{env('PRICE_SYMBOL').$vendor_product->mk_price}}
         @endif
     </td>
     <td>
         @if($vendor_product)
-            {{$vendor_product->price}}
+            {{env('PRICE_SYMBOL').$vendor_product->price}}
         @endif
     </td>
     <td>
@@ -43,6 +43,12 @@
     </td>
     <td>
         {{$content->qty}}
+    </td>
+    <td>
+        {{env('PRICE_SYMBOL').$content->grand_total}}
+    </td>
+    <td>
+        {{$content->payment_option}}
     </td>
     <td>
         {{$content->created_at->format(env('GENERAL_DATE_FORMAT_WITH_HI'))}}
