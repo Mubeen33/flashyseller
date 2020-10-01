83c7df5e9480ac588ccfcbfa2721bc29aab31005
Mubeen33 <mubeenbaig33@gmail.com>
2020-10-01 16:37:39 +0500

    sdsd

--- app/ProductVariation.php
+++ app/ProductVariation.php
@@ -7,8 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class ProductVariation extends Model
 {
     protected $table = 'product_variations';
-
-    public function product_variation(){
-    	return $this->belongsTo('App\VendorProduct', 'id', 'variation_id')->where('active', 1);
-    }
 }
