6ec13ef8c54a213447bf31bca60363f6bcd9c0ef
Mubeen33 <mubeenbaig33@gmail.com>
2020-10-13 17:08:53 +0500

    saas

--- resources/views/orders/partials/orders-list.blade.php
+++ resources/views/orders/partials/orders-list.blade.php
@@ -1,11 +1,90 @@
 @foreach($data as $key=>$content)
-<tr>    
-    <td>{{ $key }}</td>
-    @foreach($content as $order)
-    <td>{{ $order->product_name }}</td>
-    @endforeach
+<tr>
+    <td>{{ $content->order_id }}</td>
+    <td>
+        {{ $content->get_customer->first_name }} {{ $content->get_customer->last_name }}
+    </td>
+    <td>
+        <?php
+            $vendor_product = (\App\VendorProduct::where('id', $content->get_vendor_product->id)->first());
+            $product = (\App\Product::where('id', $content->get_vendor_product->prod_id)->with(['get_category', 'get_images'])->first());
+            if ($product) {
+                //get product
+                echo $product->title;
+            }
+        ?>
+    </td>
+    <td>
+        @if($product)
+            {{$product->get_category->name}}
+        @endif
+    </td>
+    <td>
+        @if($vendor_product)
+            {{env('PRICE_SYMBOL').$vendor_product->mk_price}}
+        @endif
+    </td>
+    <td>
+        @if($vendor_product)
+            {{env('PRICE_SYMBOL').$vendor_product->price}}
+        @endif
+    </td>
+    <td>
+        @if($product && count($product->get_images) > 0)
+            @foreach($product->get_images as $key_img=>$image)
+                <?php
+                    if ($key_img == 0) {
+                        echo "<img src='".$image->image."' width='80px' height='50px'>";
+                        break;
+                    }
+                ?>
+            @endforeach
+        @endif
+    </td>
+    <td>
+        {{$content->qty}}
+    </td>
+    <td>
+        {{env('PRICE_SYMBOL').$content->grand_total}}
+    </td>
+    <td>
+        {{$content->payment_option}}
+    </td>
+    <td>
+        {{$content->created_at->format(env('GENERAL_DATE_FORMAT_WITH_HI'))}}
+    </td>
+    <td>
+        <span class="badge badge-info">{{$content->status}}</span>
+    </td>
+
+@if (!empty($content->shipped))   
+    <td>
+        <span class="badge badge-info">{{$content->shipped}}</span>
+    </td>
+@else
+    <td>
+        <span class="badge badge-info">No</span>
+    </td>
+@endif    
+    <td>
+        <div class="btn-group">
+            <div class="dropdown">
+                <button class="btn btn-dark btn-sm dropdown-toggle mr-1" type="button" id="dropdownMenuButton7" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Actions
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton7">
+                    <a class="dropdown-item" href="#">Accept</a>
+
+                    @if($content->status !== "Completed" && $content->shipped != "Yes" )
+                        <a class="dropdown-item" href="{{ route('vendor.orderAction.post', [encrypt($content->id), 'Shipped']) }}">Shipped</a>
+                    @endif
+                </div>
+            </div>
+        </div>
+        
+    </td>
 </tr>
 @endforeach
 <tr>
-    <td colspan="14">{{-- {!! $data->links() !!} --}}</td>
+    <td colspan="14">{!! $data->links() !!}</td>
 </tr>
