96d64930cb78dda65a29b13c002e433c4e818707
Waqas <raowaqasmushtaq@gmail.com>
2020-12-30 11:30:49 +0500

    flashy seller
    issues fixed

--- resources/views/product/partials/auto-category.blade.php
+++ resources/views/product/partials/auto-category.blade.php
@@ -1,11 +1,12 @@
-<div class="auto-complete-wrapper">
-	<ul>
+<div class="auto-complete-wrapper" >
+	<ul >
 		@if(isset($categories))
 			@foreach($categories as $key=>$content)
-			<li getCategoryId="{{$content->id}}" gettitle="{{$content->name}}">
+			<li style="padding: 10px!important;    border: 0.5px solid  #dfdfdf;" getCategoryId="{{$content->id}}" gettitle="{{$content->name}}">
 				<span><b>{{$content->name}}</b></span><br>
 				<span>{{ $content->getParentsNames() }}</span>
 			</li>
+	
 			@endforeach
 		@endif
 	</ul>
